391ff4213a9c9497960b83a4b5220624b40ad599
Jaehwan Lee <eaw0224@korea.ac.kr>
2024-02-28 13:13:25 +0900

    h2N_run? h2N_idx

--- sample/sample_spectrum_2d.C
+++ sample/sample_spectrum_2d.C
@@ -17,22 +17,22 @@ void sample_spectrum_2d()
 	uint8_t istrip;
 
 
-	TH2I *h2N_run_ADCohmic;
-	TH2I *h2N_run_ADCstripU;
-	TH2I *h2N_run_ADCstripD;
-	h2N_run_ADCohmic = new TH2I(
-				Form("h2N_run_ADCohmic"),
-				Form("h2N_run_ADCohmic; Ohmic idx; ADC; count"),
+	TH2I *h2N_idx_ADCohmic;
+	TH2I *h2N_idx_ADCstripU;
+	TH2I *h2N_idx_ADCstripD;
+	h2N_idx_ADCohmic = new TH2I(
+				Form("h2N_idx_ADCohmic"),
+				Form("h2N_idx_ADCohmic; Ohmic idx; ADC; count"),
 				Nohmic*Ndet, 0, Nohmic*Ndet,	
 				0x10000>>3, 0,0x10000);
-	h2N_run_ADCstripU = new TH2I(
-				Form("h2N_run_ADCstripU"),
-				Form("h2N_run_ADCstripU; strip idx; ADC; count"),
+	h2N_idx_ADCstripU = new TH2I(
+				Form("h2N_idx_ADCstripU"),
+				Form("h2N_idx_ADCstripU; strip idx; ADC; count"),
 				Nstrip*Ndet/2, 0, Nstrip*Ndet/2,	
 				0x10000>>3, 0,0x10000);
-	h2N_run_ADCstripD = new TH2I(
-				Form("h2N_run_ADCstripD"),
-				Form("h2N_run_ADCstripD; strip idx; ADC; count"),
+	h2N_idx_ADCstripD = new TH2I(
+				Form("h2N_idx_ADCstripD"),
+				Form("h2N_idx_ADCstripD; strip idx; ADC; count"),
 				Nstrip*Ndet/2, 0, Nstrip*Ndet/2,	
 				0x10000>>3, 0,0x10000);
 
@@ -58,17 +58,17 @@ void sample_spectrum_2d()
 			for (ohmichit=x6hit->v_ohmichit.begin(); ohmichit!=x6hit->v_ohmichit.end(); ++ohmichit)
 			{
 				iohmic = ohmichit->idx;
-				h2N_run_ADCohmic->Fill(iohmic+ix6*Nohmic, ohmichit->peak);
+				h2N_idx_ADCohmic->Fill(iohmic+ix6*Nohmic, ohmichit->peak);
 			}
 			for (striphit=x6hit->v_strpUhit.begin(); striphit!=x6hit->v_strpUhit.end(); ++striphit)
 			{
 				istrip = striphit->idx;
-				h2N_run_ADCstripU->Fill(istrip+ix6*Nstrip/2, striphit->peak);
+				h2N_idx_ADCstripU->Fill(istrip+ix6*Nstrip/2, striphit->peak);
 			}
 			for (striphit=x6hit->v_strpDhit.begin(); striphit!=x6hit->v_strpDhit.end(); ++striphit)
 			{
 				istrip = striphit->idx;
-				h2N_run_ADCstripD->Fill(istrip+ix6*Nstrip/2, striphit->peak);
+				h2N_idx_ADCstripD->Fill(istrip+ix6*Nstrip/2, striphit->peak);
 			}
 		}
 	}
@@ -76,16 +76,16 @@ void sample_spectrum_2d()
 	TVirtualPad *pad;
 	TCanvas *c1 = new TCanvas("c1_Ohmic","c1_Ohmic",1200,800);
 	pad = c1->cd();
-	h2N_run_ADCohmic->Draw("colz");
+	h2N_idx_ADCohmic->Draw("colz");
 
 
 	TCanvas *c2 = new TCanvas("c2_StripU","c2_StripU",1200,800);
 	pad = c2->cd();
-	h2N_run_ADCstripU->Draw("colz");
+	h2N_idx_ADCstripU->Draw("colz");
 
 	TCanvas *c3 = new TCanvas("c3_StripD","c3_StripD",1200,800);
 	pad = c3->cd();
-	h2N_run_ADCstripD->Draw("colz");
+	h2N_idx_ADCstripD->Draw("colz");
 
 
 
